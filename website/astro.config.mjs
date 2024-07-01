34185486c6fe33a27c96ca8ab5f5223281d23459
Freddy L <neo-ciber_94@hotmail.com>
2024-06-30 23:08:16 -0400

    Add docs site base

---
+++ website/astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "keiro",
+      social: {
+        github: "https://github.com/Neo-Ciber94/keiro",
+      },
+      customCss: ["./src/styles/theme.css"],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
