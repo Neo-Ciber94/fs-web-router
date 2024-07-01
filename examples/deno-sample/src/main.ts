8ed646f50d6fd262b874cc72d24d345a94d18ded
Freddy L <neo-ciber_94@hotmail.com>
2024-06-30 21:58:24 -0400

    Update deno example

--- examples/deno-sample/src/main.ts
+++ examples/deno-sample/src/main.ts
@@ -1,4 +1,13 @@
 import { fileSystemRouter } from "../../../packages/keiro/src/handler/web.ts";
+import nodeProcess from "node:process";
+
+// eslint-disable-next-line no-var, @typescript-eslint/no-explicit-any
+declare var process: any;
+
+if (typeof process === "undefined") {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (globalThis as any).process = nodeProcess;
+}
 
 Deno.serve({
   handler: fileSystemRouter(),
