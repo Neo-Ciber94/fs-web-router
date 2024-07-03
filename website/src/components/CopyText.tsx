6ff81825e3bb3a71f6d30803ac740a9400b11d53
Freddy L <neo-ciber_94@hotmail.com>
2024-07-02 22:14:22 -0400

    Add copy text

---
+++ website/src/components/CopyText.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { MdContentCopy } from "react-icons/md";
+import { FaCheck } from "react-icons/fa6";
+type CopyTextProps = {
+  text: string;
+  className?: string;
+};
+
+export default function CopyText({ text, className }: CopyTextProps) {
+  const [wasCopied, setWasCopied] = useState(false);
+  const [showFeedback, setShowFeedback] = useState(false);
+
+  useEffect(() => {
+    if (!wasCopied) {
+      return;
+    }
+
+    const timeout1 = setTimeout(() => setWasCopied(false), 2000);
+
+    return () => {
+      clearInterval(timeout1);
+    };
+  }, [wasCopied, showFeedback]);
+
+  useEffect(() => {
+    if (!showFeedback) {
+      return;
+    }
+
+    const timeout2 = setTimeout(() => setShowFeedback(false), 1200);
+
+    return () => {
+      clearInterval(timeout2);
+    };
+  }, [wasCopied, showFeedback]);
+
+  return (
+    <button
+      className={`p-2 border border-neutral-500 bg-neutral-300/10 rounded-md flex flex-row justify-center items-center opacity-50 hover:opacity-100 cursor-pointer transition duration-300 
+        ${className} ${wasCopied ? "!bg-cyan-400/60 !opacity-100" : ""}`}
+      onClick={() => {
+        navigator.clipboard
+          .writeText(text)
+          .then(() => {
+            setWasCopied(true);
+            setShowFeedback(true);
+          })
+          .catch(console.error);
+      }}
+    >
+      {showFeedback ? <FaCheck size={20} /> : <MdContentCopy size={20} />}
+    </button>
+  );
+}
