e475766dad97ea5e50d1094bc50e674980a5ac68
Freddy L <neo-ciber_94@hotmail.com>
2024-07-05 01:36:55 -0400

    Add fixed and dynamic worker pool

--- packages/keiro/src/workers/workerPool.ts
+++ packages/keiro/src/workers/workerPool.ts
@@ -1,5 +1,7 @@
 import { Worker, type WorkerOptions } from "node:worker_threads";
 
+// https://github.com/vitest-dev/vitest/issues/5757#issuecomment-2126095729
+
 const jsWorker = /* JavaScript */ `
   import { createRequire } from "node:module";
   import { workerData, threadId } from "node:worker_threads";
@@ -21,11 +23,27 @@ class TsxWorker extends Worker {
 
 type QueueWorker = (worker: Worker) => void;
 
-export class WorkerPool {
+export abstract class WorkerPool {
+  /**
+   * Returns a promise that resolves to the next available `Worker`.
+   */
+  abstract take(): Promise<Worker>;
+
+  /**
+   * Returns a worker to the pool.
+   */
+  abstract return(worker: Worker): void;
+}
+
+const FIXED_POOL_WORKER = Symbol("FIXED_POOL_WORKER");
+
+export class FixedWorkerPool extends WorkerPool {
   #workers: Worker[];
   #queue: QueueWorker[];
 
   constructor(workerCount: number, filename: string | URL, options?: WorkerOptions) {
+    super();
+
     if (workerCount <= 0) {
       throw new Error("Worker count cannot be zero or negative");
     }
@@ -33,11 +51,31 @@ export class WorkerPool {
     const workers: Worker[] = [];
 
     for (let i = 0; i < workerCount; i++) {
-      workers.push(new TsxWorker(filename, options));
+      const worker = new TsxWorker(filename, options);
+
+      // This is to detect which workers comes from this pool
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (worker as any)[FIXED_POOL_WORKER] = true;
+
+      workers.push(worker);
     }
 
     this.#workers = workers;
     this.#queue = [];
+  }
+
+  /**
+   * Returns the number of workers that will available to run tasks.
+   */
+  get available() {
+    return this.#workers.length;
+  }
+
+  /**
+   * Returns the total number of workers this pool contain.
+   */
+  get size() {
+    return this.available + this.#queue.length;
   }
 
   take() {
@@ -51,13 +89,84 @@ export class WorkerPool {
     });
   }
 
-  // This is an unsafe operation, we don't know if the worker actually is part of this pool
   return(worker: Worker) {
+    if (!(FIXED_POOL_WORKER in worker)) {
+      return;
+    }
+
     if (this.#queue.length > 0) {
       const resolve = this.#queue.pop()!;
       resolve(worker);
     } else {
       this.#workers.unshift(worker);
     }
+  }
+}
+
+const DYNAMIC_POOL_WORKER = Symbol("DYNAMIC_POOL_WORKER");
+
+export class DynamicWorkerPool extends WorkerPool {
+  #workers: Worker[];
+  #minWorkerCount: number;
+  #workerThreadCount: number;
+
+  // options
+  #workerFilename: string | URL;
+  #workerOptions: WorkerOptions | undefined;
+
+  constructor(initialWorkerCount: number, filename: string | URL, options?: WorkerOptions) {
+    super();
+
+    if (initialWorkerCount <= 0) {
+      throw new Error("Worker count cannot be zero or negative");
+    }
+
+    const workers: Worker[] = [];
+
+    for (let i = 0; i < initialWorkerCount; i++) {
+      const worker = new TsxWorker(filename, options);
+
+      // This is to detect which workers comes from this pool
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (worker as any)[DYNAMIC_POOL_WORKER] = true;
+
+      workers.push(worker);
+    }
+
+    this.#workers = workers;
+    this.#minWorkerCount = initialWorkerCount;
+    this.#workerFilename = filename;
+    this.#workerOptions = options;
+    this.#workerThreadCount = initialWorkerCount;
+  }
+
+  take(): Promise<Worker> {
+    const worker = this.#workers.pop();
+
+    if (worker) {
+      this.#workerThreadCount -= 1;
+      return Promise.resolve(worker);
+    }
+
+    const newWorker = new TsxWorker(this.#workerFilename, this.#workerOptions);
+
+    // This is to detect which workers comes from this pool
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (newWorker as any)[DYNAMIC_POOL_WORKER] = true;
+    return Promise.resolve(newWorker);
+  }
+
+  return(worker: Worker): void {
+    if (!(DYNAMIC_POOL_WORKER in worker)) {
+      return;
+    }
+
+    // If we are full, we ignore the worker
+    if (this.#workerThreadCount >= this.#minWorkerCount) {
+      return;
+    }
+
+    this.#workerThreadCount += 1;
+    this.#workers.push(worker);
   }
 }
